6c536b6110f4fdb3466cea09d621e24f5cd82e09
Nicklamn0 <159081142+Nicklamn0@users.noreply.github.com>
2024-02-23 22:02:25 -0500

    Updated a lot
    Idk I did a lot though

--- Source/Nicksapp/ContentView.swift
+++ Source/Nicksapp/ContentView.swift
@@ -1,23 +1,38 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showDetails = false
+    let ts: String = "apple-magnifier://"
+    let sl: String = "sileo://"
+    let zb: String = "zbra://"
+    let set: String = "com.apple.preferences://"
+    let myapp: String = "nicksapp://"
 
     var body: some View {
-       Text("Test!")
-       Text("New Line!")
-       Button {
-             print("Button pressed")
-                 showDetails.toggle()
-             } label: {
-                 Text("Open me!")
-             .padding(10)
+       Text("Open Apps!\n")
+       Text("Needed if they UI Cache\n")
+       Text("Lmao swift is awesome\n")
+       Text("ONLY works with URL Schemes enable!\n")
+       Button("Open TrollStore\n") {
+             if let url = URL(string: ts) {
+    UIApplication.shared.open(url)
+}
        }
-       .contentShape(Rectangle())
-        if showDetails {
-                Text("Thanks for opening me")
+       Button("Open Sileo\n") {
+             if let url = URL(string: sl) {
+    UIApplication.shared.open(url)
+}
        }
-
-
+       Button("Open Zebra\n") {
+             if let url = URL(string: zb) {
+    UIApplication.shared.open(url)
+}
+       }
+       Button("Open Settings") {
+             if let url = URL(string: set) {
+    UIApplication.shared.open(url)
+}
+       }
+       
+       
     }
 }
